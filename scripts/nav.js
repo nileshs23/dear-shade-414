cffa48ae5f97743e28ebc2de4d222178b66df553
nileshs23 <nileshs2398@gmail.com>
2022-07-20 11:48:55 +0530

    Navbar completed

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,16 +1,20 @@
-window.onscroll = function() {myFunction()};
-
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
+import nav from "../components/navbar.js";
+navbar.innerHTML = nav();
+
+// scrolling function to navbar 
+window.onscroll = function () {
+	myFunction();
+};
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
-
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
+	}
 }
