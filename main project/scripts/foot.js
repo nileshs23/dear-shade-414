12ed435e0325bec93234a8a3be5877828765c070
nileshs23 <nileshs2398@gmail.com>
2022-07-24 11:41:04 +0530

    Fresh to mergre

--- main project/scripts/foot.js
+++
@@ -1,4 +0,0 @@
-import footer from "../components/footer.js"
-// console.log(footer);
-
-document.getElementById("footer1").innerHTML = footer();
--- scripts/appSidebar.js
+++
@@ -1,3 +0,0 @@
-import sbar from "../components/sidebar.js";
-let sidebar = document.getElementById("sidebar");
-sidebar.innerHTML = sbar;
